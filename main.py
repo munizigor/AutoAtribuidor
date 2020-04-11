f7c24d1391e353bab257b943fc7aac58098fd174
¨Igor <muniz.igor@gmail.com>
2020-04-11 10:43:35 -0300

    Atualizacao 2 de estrutura

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.options import Options
 import time
 from bs4 import BeautifulSoup
-# import datetime
-# import re
 import getpass
 import pickle
 
@@ -13,9 +11,10 @@ class Servidor():
         self.matricula = matricula
 
 class Operador(Servidor):
-    def __init__(self,matricula):
+    def __init__(self,matricula,setor):
         #Recebe a matricula já definida na Parent Class
         super().__init__(matricula)
+        self.setor = setor
 class Processo():
     def __init__(self,processo):
         self.processo = processo
@@ -23,7 +22,6 @@ class Processo():
 class Browser():
     def start(self):
         print('Iniciando o programa...\n\n')
-
         # Entrar no SEI
         usuario = input('Informe sua matrícula: ')
         password = getpass.getpass('Informe a Senha: ')
@@ -38,25 +36,34 @@ class Browser():
         Select(driver.find_element_by_id('selOrgao')).select_by_value('7')
         print('Logando no SEI...\n\n')
         driver.find_element_by_id('sbmLogin').click()
+        #TODO:Se erro de login
         time.sleep(3)
-        processos_recebidos = driver.find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
-        lista_processos = processos_recebidos.text
+        processos_recebidos = driver.find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name('tbody')
         processos_recebidos_innerHTML = BeautifulSoup(processos_recebidos.get_attribute("innerHTML"), "html.parser")
-
-        print(lista_processos)
-        print(processos_recebidos_innerHTML)
-
+        processos_list=processos_recebidos_innerHTML.find_all('tr')
 Browser().start()
 # TODO: Estruturar os dados abaixo
-# with open('processos_caixa_entrada.obj','w') as file_pi:
-#     for idx, processo in enumerate(processos_recebidos_innerHTML[1:]):
-#         processo_pi = Processo(processos_recebidos[count].children[2].getElementsByTagName('a')[0].innerHTML)
-#         processo_pi.checkbox = processos_recebidos[idx].find_element_by_tag_name('input')
+def entre_parenteses(x):
+    return x[x.find("(")+1:x.rfind(")")]
+with open('processos_caixa_entrada.obj','w') as file_pi:
+    for processo in processos_list[1:]:
+        children = processo.find_all('td')
+        processo_pi = Processo(children[2].text)
+        processo_pi.checkbox = processos_recebidos.find_element_by_id(children[0].find('input').get('id'))
+        processo_pi.visualizado = 'processoVisualizado' in children[2].find('a')['class']
+        processo_pi.visitado = 'processoVisitado' in children[2].find('a')['class']
+        processo_pi.descricao = entre_parenteses(children[2].find('a')['onmouseover'])
+        processo_pi.link = 'https://sei.df.gov.br/sei/' + children[2].find('a')['href']
+        processo_pi.matricula_operador = entre_parenteses(children[3].text)
+        pickle.dump(processo_pi, file_pi)
+            #TODO: Continuar os de baixo
+            #TODO: O children[1] pode conter até quatro <a>. Primeira Anotação, segunda informacao de assinatura e a terceira a tag. Deve-se entao pegar o getAttribute('onmouseover') de todos os <a> que esse children tiver
+            #TODO: Usar urllib.parse para pegar parametros GET
+
+#         processo_pi.anotacao = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
+#         processo_pi.bool_assinado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
+#         processo_pi.retorno_programado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
 #         processo_pi.tag = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         processo_pi.visualizado = processos_recebidos[count].children[2].getElementsByTagName('a')[0].getAttribute('class')
-#         processo_pi.descricao = processos_recebidos[count].children[2].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         processo_pi.link = 'https://sei.df.gov.br/sei/'+str(processos_recebidos[count].children[2].getElementsByTagName('a')[0].getAttribute('href'))
-#         processo_pi.matricula_operador = processos_recebidos[count].children[3].getElementsByTagName('a')[0].innerHTML
 #         pickle.dump(processo_pi, file_pi)
 
 
