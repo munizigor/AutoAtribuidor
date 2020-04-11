a7c8e6c44cdce494c1bd9494887779fd39b13f25
¨Igor <muniz.igor@gmail.com>
2020-04-11 18:12:04 -0300

    Estruturacao de classes

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ from selenium.webdriver.firefox.options import Options
 import time
 from bs4 import BeautifulSoup
 import getpass
-import pickle
+import unicodedata
+import re
 
 class Servidor():
     def __init__(self,matricula):
         self.matricula = matricula
+    def __str__(self):
+        return self.matricula
 
 class Operador(Servidor):
     def __init__(self,matricula,setor):
         #Recebe a matricula já definida na Parent Class
         super().__init__(matricula)
         self.setor = setor
+    def __str__(self):
+        return self.matricula+' - '+self.setor
 class Processo():
     def __init__(self,processo):
         self.processo = processo
-
+    def __str__(self):
+        return self.processo+' - '+self.descricao
 class Browser():
-    def start(self):
+    def login(self):
         print('Iniciando o programa...\n\n')
         # Entrar no SEI
         usuario = input('Informe sua matrícula: ')
@@ -38,24 +44,30 @@ class Browser():
         driver.find_element_by_id('sbmLogin').click()
         #TODO:Se erro de login
         time.sleep(3)
-        processos_recebidos = driver.find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name('tbody')
+        return driver
+class ProcessoHandler(Browser):
+    def processos_list(self):
+        processos_recebidos = super().login().find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name('tbody')
         processos_recebidos_innerHTML = BeautifulSoup(processos_recebidos.get_attribute("innerHTML"), "html.parser")
-        processos_list=processos_recebidos_innerHTML.find_all('tr')
-Browser().start()
-# TODO: Estruturar os dados abaixo
-def entre_parenteses(x):
-    return x[x.find("(")+1:x.rfind(")")]
-with open('processos_caixa_entrada.obj','w') as file_pi:
-    for processo in processos_list[1:]:
-        children = processo.find_all('td')
-        processo_pi = Processo(children[2].text)
-        processo_pi.checkbox = processos_recebidos.find_element_by_id(children[0].find('input').get('id'))
-        processo_pi.visualizado = 'processoVisualizado' in children[2].find('a')['class']
-        processo_pi.visitado = 'processoVisitado' in children[2].find('a')['class']
-        processo_pi.descricao = entre_parenteses(children[2].find('a')['onmouseover'])
-        processo_pi.link = 'https://sei.df.gov.br/sei/' + children[2].find('a')['href']
-        processo_pi.matricula_operador = entre_parenteses(children[3].text)
-        pickle.dump(processo_pi, file_pi)
+        processos = processos_recebidos_innerHTML.find_all('tr')
+        return processos,processos_recebidos
+    def selecionar(self):
+        processos = self.processos_list()
+        for processo in processos[0][1:]:
+            children = processo.find_all('td')
+            processo_pi = Processo(children[2].text)
+            processo_pi.checkbox = processos[1].find_element_by_id(children[0].find('input').get('id'))
+            processo_pi.visualizado = 'processoVisualizado' in children[2].find('a')['class']
+            processo_pi.visitado = 'processoVisitado' in children[2].find('a')['class']
+            processo_pi.descricao = limpastr(entre_parenteses(children[2].find('a')['onmouseover'])).lower()
+            processo_pi.link = 'https://sei.df.gov.br/sei/' + children[2].find('a')['href']
+            processo_pi.matricula_operador = entre_parenteses(children[3].text)
+            # pickle.dump(processo_pi, file_pi)
+            print(processo_pi)
+            if ('dependente' in processo_pi.descricao) or ('beneficiario' in processo_pi.descricao) or (
+                    'luto' in processo_pi.descricao) or ('funeral' in processo_pi.descricao) or (
+                    'moradia' in processo_pi.descricao):
+                processo_pi.checkbox.click()
             #TODO: Continuar os de baixo
             #TODO: O children[1] pode conter até quatro <a>. Primeira Anotação, segunda informacao de assinatura e a terceira a tag. Deve-se entao pegar o getAttribute('onmouseover') de todos os <a> que esse children tiver
             #TODO: Usar urllib.parse para pegar parametros GET
@@ -65,45 +77,34 @@ with open('processos_caixa_entrada.obj','w') as file_pi:
 #         processo_pi.retorno_programado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
 #         processo_pi.tag = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
 #         pickle.dump(processo_pi, file_pi)
+    def atribuir (self):
+        pass
+    def arquivar (self):
+        pass
+    def etiquetar (self):
+        pass
+    def enviar (self):
+        pass
+    def atualizar_andamento (self):
+        pass
+    def concluir (self):
+        pass
+    def inserir_documento (self):
+        pass
+    def inserir_anotacao (self):
+        pass
 
+def entre_parenteses(x):
+    return x[x.find("(")+1:x.rfind(")")]
+def limpastr(palavra):
+    # Unicode normalize transforma um caracter em seu equivalente em latin.
+    if palavra != None:
+        nfkd = unicodedata.normalize('NFKD', palavra)
+    else:
+        nfkd = ''
+    palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
 
-# Caso queira aproveitar o resto do código #
+    # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
+    return re.sub('[^a-zA-Z0-9 ,\'\\\]', '', palavraSemAcento)
 
-# def get_req(num):
-# 	driver.find_element_by_id('txtPesquisaRapida').send_keys(num)
-# 	driver.find_element_by_id('txtPesquisaRapida').submit()
-# 	time.sleep(3)
-# 	# TODO: Pegar data remetimento do requerimento
-# 	driver.switch_to.frame(driver.find_element_by_name('ifrVisualizacao'))
-# 	try:
-# 		driver.switch_to.frame(driver.find_element_by_id('ifrArvoreHtml'))
-# 	except selenium.common.exceptions.NoSuchElementException:
-# 		time.sleep(7)
-# 		driver.switch_to.frame(driver.find_element_by_id('ifrArvoreHtml'))
-# 	elem = driver.find_element_by_id("conteudo")
-# 	source_code = BeautifulSoup(elem.get_attribute("innerHTML"), "html.parser")
-# 	text = source_code.get_text()
-# 	return text
-
-#Acessar o requerimento
-# time.sleep(3)
-# print('Acessando o requerimento...\n\n')
-# text = get_req(q)
-# driver.quit()
-# # print(text)
-# dados = re.split('\n\n|:\n\xa0\xa0|\n\xa0\xa0|\n', text)
-# rmv = ['',
-#        'DADOS DO DEPENDENTE:',
-#        'DADOS DA CERTIDÃO NOVA (PREENCHER APENAS SE SUA CERTIDÃO FOR DO MODELO NOVO):',
-#        'DADOS DA CERTIDÃO ANTIGA (PREENCHER APENAS SE SUA CERTIDÃO FOR DO MODELO ANTIGO):',
-#        'DADOS DOS PAIS DO BENEFICIÁRIO:',
-#        'REQUEIRO, na forma da Lei, o RECONHECIMENTO do dependente especificado neste documento  junto à Corporação, além da concessão dos benefícios a seguir:',
-#        'DECLARAÇÕES (PREENCHIMENTO OBRIGATÓRIO):']
-# result = list(filter(lambda x: not (x in rmv), dados))
-# # print(result)
-# dados_dict = {result[2 * n]: result[2 * n + 1] for n in range(int(len(result) // 2))}
-#
-# try:
-# 	driver.find_element_by_xpath('titulos')
-# except WebDriverException:
-# 	print(False)
+ProcessoHandler().selecionar()
