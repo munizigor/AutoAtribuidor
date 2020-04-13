0dd71095a641485ea09e16b456bf9038a99e1cdd
¨Igor <muniz.igor@gmail.com>
2020-04-13 08:52:00 -0300

    novas features

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoAlertPresentException, JavascriptException, NoSuchElementException
 import time
 from bs4 import BeautifulSoup
 import getpass
 import unicodedata
 import re
+import csv
 
 class Servidor():
     def __init__(self,matricula):
@@ -46,37 +49,89 @@ class Browser():
         time.sleep(3)
         return driver
 class ProcessoHandler(Browser):
-    def processos_list(self):
-        processos_recebidos = super().login().find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name('tbody')
-        processos_recebidos_innerHTML = BeautifulSoup(processos_recebidos.get_attribute("innerHTML"), "html.parser")
-        processos = processos_recebidos_innerHTML.find_all('tr')
-        return processos,processos_recebidos
-    def selecionar(self):
-        processos = self.processos_list()
-        for processo in processos[0][1:]:
-            children = processo.find_all('td')
-            processo_pi = Processo(children[2].text)
-            processo_pi.checkbox = processos[1].find_element_by_id(children[0].find('input').get('id'))
-            processo_pi.visualizado = 'processoVisualizado' in children[2].find('a')['class']
-            processo_pi.visitado = 'processoVisitado' in children[2].find('a')['class']
-            processo_pi.descricao = limpastr(entre_parenteses(children[2].find('a')['onmouseover'])).lower()
-            processo_pi.link = 'https://sei.df.gov.br/sei/' + children[2].find('a')['href']
-            processo_pi.matricula_operador = entre_parenteses(children[3].text)
-            # pickle.dump(processo_pi, file_pi)
-            print(processo_pi)
-            if ('dependente' in processo_pi.descricao) or ('beneficiario' in processo_pi.descricao) or (
-                    'luto' in processo_pi.descricao) or ('funeral' in processo_pi.descricao) or (
-                    'moradia' in processo_pi.descricao):
-                processo_pi.checkbox.click()
-            #TODO: Continuar os de baixo
-            #TODO: O children[1] pode conter até quatro <a>. Primeira Anotação, segunda informacao de assinatura e a terceira a tag. Deve-se entao pegar o getAttribute('onmouseover') de todos os <a> que esse children tiver
-            #TODO: Usar urllib.parse para pegar parametros GET
+    def abrir_processos(self,filepath):
+        driver = super().login()
+        with open(filepath,'r') as processos:
+            processos_reader = csv.reader(processos)
+            for line in processos_reader:
+                if line[0] == '':
+                    continue
+                else:
+                    try:
+                        driver.find_element_by_id('txtPesquisaRapida').send_keys(line[0],Keys.ENTER)
+                    except NoSuchElementException:
+                        time.sleep(10)
+                        driver.find_element_by_id('txtPesquisaRapida').send_keys(line[0], Keys.ENTER)
+                # driver.find_element_by_id('txtPesquisaRapida').send_keys('00053-00088071/2018-01',Keys.ENTER)
+                    time.sleep(3)
+                    try:
+                        driver.switch_to.frame('ifrVisualizacao')
+                    except NoSuchElementException:
+                        time.sleep(20)
+                        driver.switch_to.frame('ifrVisualizacao')
+                    try:
+                        driver.execute_script('reabrirProcesso()')
+                    except JavascriptException:
+                        time.sleep(15)
+                        driver.execute_script('reabrirProcesso()')
+                    time.sleep(3)
+                    try:
+                        alert = driver.switch_to.alert
+                        assert 'Processo já está aberto na unidade atual.' in alert.text or 'Processo está anexado.' in alert.text
+                        alert.accept()
+                    except NoAlertPresentException:
+                        pass
+                    driver.switch_to.default_content()
 
-#         processo_pi.anotacao = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         processo_pi.bool_assinado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         processo_pi.retorno_programado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         processo_pi.tag = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
-#         pickle.dump(processo_pi, file_pi)
+    def ler_processos(self):
+        driver = super().login()
+        # return driver
+        with open('processos_recebidos.csv', mode='w') as processos_save:
+            processos_writer = csv.writer(processos_save)
+            processos_writer.writerow(['Processo','Descricao','Anotacoes','Tag'])
+            while True:
+                processos_recebidos = driver.find_element_by_id('tblProcessosRecebidos').find_element_by_tag_name(
+                    'tbody')
+                processos_recebidos_innerHTML = BeautifulSoup(processos_recebidos.get_attribute("innerHTML"),
+                                                              "html.parser")
+                processos = processos_recebidos_innerHTML.find_all('tr')
+                for processo in processos[1:]:
+                    children = processo.find_all('td')
+                    processo_pi = Processo(children[2].text)
+                    # processo_pi.checkbox = processos_recebidos.find_element_by_id(children[0].find('input').get('id'))
+                    processo_pi.anotacao = ''
+                    processo_pi.tag = ''
+                    if len(children[1].find_all('a'))==0:
+                        pass
+                    else:
+                        for child in children[1].find_all('a'):
+                            processo_pi.anotacao = entre_parenteses(child['onmouseover']) if (
+                                        'anotacao_registrar' in child['href']) else processo_pi.anotacao
+                            processo_pi.tag = entre_parenteses(child['onmouseover']) if (
+                                        'andamento_marcador_gerenciar' in child['href']) else processo_pi.tag
+                    # processo_pi.visualizado = 'processoVisualizado' in children[2].find('a')['class']
+                    # processo_pi.visitado = 'processoVisitado' in children[2].find('a')['class']
+                    processo_pi.descricao = limpastr(entre_parenteses(children[2].find('a')['onmouseover'])).lower()
+                    # processo_pi.link = 'https://sei.df.gov.br/sei/' + children[2].find('a')['href']
+                    # processo_pi.matricula_operador = entre_parenteses(children[3].text)
+                    # if ('dependente' in processo_pi.descricao) or ('beneficiario' in processo_pi.descricao) or (
+                    #         'luto' in processo_pi.descricao) or ('funeral' in processo_pi.descricao) or (
+                    #         'moradia' in processo_pi.descricao):
+                    #     processo_pi.checkbox.click()
+
+                    processos_writer.writerow(
+                        [processo_pi.processo, processo_pi.descricao, processo_pi.anotacao, processo_pi.tag])
+                if len(driver.find_elements_by_id('lnkRecebidosProximaPaginaSuperior')) != 0:
+                    driver.find_element_by_id('lnkRecebidosProximaPaginaSuperior').click()
+                    time.sleep(5)
+                else:
+                    return
+                #TODO: Continuar os de baixo
+                #TODO: Usar urllib.parse para pegar parametros GET
+
+    #         processo_pi.bool_assinado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
+    #         processo_pi.retorno_programado = processos_recebidos[count].children[1].getElementsByTagName('a')[0].getAttribute('onmouseover')
+    #         pickle.dump(processo_pi, file_pi)
     def atribuir (self):
         pass
     def arquivar (self):
@@ -94,6 +149,30 @@ class ProcessoHandler(Browser):
     def inserir_anotacao (self):
         pass
 
+class Pesquisa(Browser):
+    def ler_processos(self,cod_unidade):
+        driver = super().login()
+        driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/ul/li[4]/a').click()
+        time.sleep(3)
+        driver.execute_script('document.getElementById(\'hdnIdUnidade\').value='+cod_unidade)
+        driver.find_element_by_id('sbmPesquisar').click()
+        return driver
+    def salvar_processos(self,cod_unidade):
+        driver = self.ler_processos(cod_unidade)
+        limit_count = driver.find_elements_by_class_name('barra')[0].text.split(' ')
+        limit_count = int(limit_count[-1])
+        print(limit_count)
+        count=0
+        with open('processos_save.csv', mode='w') as processos_save:
+            processos_writer = csv.writer(processos_save)
+            processos_writer.writerow(['Título', 'Processo'])
+            while count<limit_count:
+                for elem in driver.find_elements_by_class_name('resTituloEsquerda'):
+                    processos_writer.writerow([elem.text,elem.text[elem.text.find("º")+2:elem.text.rfind("(")-1]])
+                count+=10
+                driver.execute_script('navegar('+str(count)+')')
+                time.sleep(7)
+
 def entre_parenteses(x):
     return x[x.find("(")+1:x.rfind(")")]
 def limpastr(palavra):
@@ -107,4 +186,6 @@ def limpastr(palavra):
     # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
     return re.sub('[^a-zA-Z0-9 ,\'\\\]', '', palavraSemAcento)
 
-ProcessoHandler().selecionar()
+# ProcessoHandler().abrir_processos('processos_sample2.csv')
+teste = ProcessoHandler().ler_processos()
+# Pesquisa().salvar_processos('110001369')
